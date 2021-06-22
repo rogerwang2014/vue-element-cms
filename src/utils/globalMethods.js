ef790bd015998c2e941447e3ea213a114696d3df
rogerwang <rogerwang2014@gmail.com>
2021-06-22 08:14:16 +0800

    globalMethods优化

--- src/utils/globalMethods.js
+++ src/utils/globalMethods.js
@@ -3,7 +3,11 @@
  */
 
 class GlobalMethods {
-    /** 防抖 */
+    /**
+     * 防抖
+     * @param fn: {Function} 待执行函数
+     * @param delay: {Number} 延迟执行的时间
+     */
     static debounce (fn, delay) {
         let timer = null
 
@@ -17,22 +21,31 @@ class GlobalMethods {
         }
     }
 
-    /** 节流 */
-    static throttle (fn, delay) {
-        let timer = null
-        let startTime = new Date().getTime()
+    /**
+     * 节流
+     * @param fn: {Function} 待执行函数
+     * @param delay: {Number} 延迟执行的时间
+     * @param immediate: {Boolean} 首次触发是否立即执行
+     */
+    static throttle (fn, delay, immediate) {
+        let canExcute = true // 是否可执行
 
         return function () {
             const context = this
             const args = arguments
-            const currentTime = new Date().getTime()
-            const remainning = delay - (currentTime - startTime)
-            clearTimeout(timer)
-            if (remainning <= 0) {
+
+            if (immediate) {
+                immediate = false
                 fn.apply(context, args)
-                startTime = new Date().getTime()
-            } else {
-                timer = setTimeout(fn, remainning)
+                return
+            }
+
+            if (canExcute) {
+                canExcute = false
+                setTimeout(() => {
+                    fn.apply(context, args)
+                    canExcute = true
+                }, delay)
             }
         }
     }
